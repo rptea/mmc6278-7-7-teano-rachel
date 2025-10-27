8b063a4943ca1e0cc8d49a60f0fea6f419f736e6
Rachel Teano <rpteano@gmail.com>
2025-10-26 20:14:59 -0400

    fixed typo in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 function checkAuth(req, res, next) {
 	// Check req.session.loggedIn to see if the user is logged in
 	// If the user is logged in, call next()
-	if (req.session && req.session.isLoggedIn) {
+	if (req.session && req.session.loggedIn) {
 		return next()
 	}
 	// If the user is not logged in, redirect to /login
